63b9b6f5f472b2b6ee7278d1b33f74f536c3d1ef
Haim Ashkenazi <haim@babysnakes.org>
2021-04-16 09:49:26 +0300

    Log provisioning

---
+++ src/RoonTagger.Cli.Test/LoggingTests.fs
@@ -0,0 +1,54 @@
+namespace RoonTagger.Cli.Test
+
+open NUnit.Framework
+open FsUnit
+open Serilog.Events
+open RoonTagger.Cli.Commands.Main
+open RoonTagger.Cli.Configuration
+
+[<TestFixture>]
+type LoggingTests() =
+
+    static member loggingEnabledInput() =
+        [ let defaultLC =
+              { File = "file.log"
+                Level = LogLevel.None }
+
+          (defaultLC,
+           1,
+           [ (LogEventLevel.Information, true)
+             (LogEventLevel.Debug, false) ])
+
+          ({ defaultLC with Level = LogLevel.Info },
+           0,
+           [ (LogEventLevel.Information, true)
+             (LogEventLevel.Debug, false) ])
+
+          (defaultLC,
+           2,
+           [ (LogEventLevel.Debug, true)
+             (LogEventLevel.Verbose, false) ])
+
+          ({ defaultLC with
+                 Level = LogLevel.Debug },
+           0,
+           [ (LogEventLevel.Debug, true)
+             (LogEventLevel.Verbose, false) ])
+
+          (defaultLC, 3, [ (LogEventLevel.Verbose, true) ])
+          (defaultLC, 5, [ (LogEventLevel.Verbose, true) ]) ]
+
+    [<TestCaseSource(nameof (LoggingTests.loggingEnabledInput))>]
+    member this.``Test log level calculation with logging enabled``
+        ((lc: LogConfigV1, overrides: int, testCases: (LogEventLevel * bool) list))
+        =
+        let logger = setupLogger lc overrides |> Option.get
+
+        for case in testCases do
+            let (level, enabled) = case
+            logger.IsEnabled level |> should equal enabled
+
+    [<Test>]
+    member this.``Test log level calculation with logging disabled`` () =
+        setupLogger { File = "file.log"; Level = LogLevel.None } 0
+        |> should equal None
--- src/RoonTagger.Cli/Commands/MainCmd.fs
+++ src/RoonTagger.Cli/Commands/MainCmd.fs
@@ -2,6 +2,8 @@ module RoonTagger.Cli.Commands.Main
 
 open System
 open Argu
+open FsToolkit.ErrorHandling
+open Serilog
 open RoonTagger.Cli
 open RoonTagger.Cli.Arguments
 open RoonTagger.Cli.Commands
@@ -15,24 +17,51 @@ let (|ViewCmd|_|) (opts: ParseResults<MainArgs>) = opts.TryGetResult View
 let (|CreditsCmd|_|) (opts: ParseResults<MainArgs>) = opts.TryGetResult Credits
 let (|ConfigureCmd|_|) (opts: ParseResults<MainArgs>) = opts.TryGetResult Configure
 
+let setupLogger (lc: LogConfigV1) (overrides: int) =
+    let level =
+        match overrides with
+        | 0 -> lc.Level
+        | 1 -> LogLevel.Info
+        | 2 -> LogLevel.Debug
+        | 3
+        | _ -> LogLevel.Trace
+
+    let f () =
+        LoggerConfiguration().WriteTo.File(lc.File)
+
+    match level with
+    | LogLevel.None -> None
+    | LogLevel.Info -> Some(f().MinimumLevel.Information().CreateLogger())
+    | LogLevel.Debug -> Some(f().MinimumLevel.Debug().CreateLogger())
+    | LogLevel.Trace
+    | _ -> Some(f().MinimumLevel.Verbose().CreateLogger())
+
 let handleCmd (opts: ParseResults<MainArgs>) =
-    let appDir = getConfigDirectory ()
-    let configFile = getConfigFilePath appDir "config" ConfigurationVersion.V1
+    result {
+        let appDir = getConfigDirectory ()
+        let configFile = getConfigFilePath appDir "config" ConfigurationVersion.V1
+        let! config = loadConfigWithDefault configFile
+        let verbosity = opts.GetResults Verbose |> List.length
 
-    Console.WriteLine("")
+        setupLogger config.Log verbosity
+        |> Option.map
+            (fun logger ->
+                Console.WriteLine ""
+                infoMessage $"Logs will be written to '{config.Log.File}'"
+                Log.Logger <- logger)
+        |> ignore
 
-    if (opts.Contains Verbose) then
-        Console.WriteLine($"DEBUG: args: {opts}")
         Console.WriteLine("")
 
-
-    match opts with
-    | VersionCmd _ -> infoMessage $"{Info.Name}: {Info.Version}" |> Ok
-    | SetTagsCmd args -> SetTags.handleCmd args
-    | EditTitlesCmd args -> EditTitles.handleCmd args
-    | ViewCmd args -> View.handleCmd args
-    | CreditsCmd args -> Credits.handleCmd args
-    | ConfigureCmd args -> Configure.handleCmd args configFile
-    | _ ->
-        handleOutput "Move along, nothing to see here..."
-        |> Ok
+        match opts with
+        | VersionCmd _ -> return infoMessage $"{Info.Name}: {Info.Version}" |> Ok
+        | SetTagsCmd args -> return SetTags.handleCmd args
+        | EditTitlesCmd args -> return EditTitles.handleCmd args
+        | ViewCmd args -> return View.handleCmd args
+        | CreditsCmd args -> return Credits.handleCmd args
+        | ConfigureCmd args -> return Configure.handleCmd args configFile
+        | _ ->
+            return
+                handleOutput "Move along, nothing to see here..."
+                |> Ok
+    }
