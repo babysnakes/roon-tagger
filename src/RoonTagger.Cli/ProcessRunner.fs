209ba472987c8d3faeeda6e5614054272673241f
Haim Ashkenazi <haim@babysnakes.org>
2021-04-18 00:49:48 +0300

    No need to search in path is Cmd is absolute path ...

--- src/RoonTagger.Cli/ProcessRunner.fs
+++ src/RoonTagger.Cli/ProcessRunner.fs
@@ -9,19 +9,26 @@ let log = Serilog.Log.Logger
 let searchInPath (cmd: string) =
     log.Debug("Searching for '{Cmd}' in PATH ...", cmd)
 
-    match (Environment.GetEnvironmentVariable "PATH" |> Option.ofObj) with
-    | None -> cmd
-    | Some path ->
-        path
-        |> fun s -> s.Split ";"
-        |> Array.map (fun d -> IO.Path.Combine(d, cmd))
-        |> Array.tryFind IO.File.Exists
-        |> function
-        | Some exe -> exe
+    let find (cmd: string) =
+        match (Environment.GetEnvironmentVariable "PATH" |> Option.ofObj) with
         | None -> cmd
-        |> fun exe ->
-            log.Debug("Search in path result: {Exe}", exe)
-            exe
+        | Some path ->
+            path
+            |> fun s -> s.Split ";"
+            |> Array.map (fun d -> IO.Path.Combine(d, cmd))
+            |> Array.tryFind IO.File.Exists
+            |> function
+            | Some exe -> exe
+            | None -> cmd
+            |> fun exe ->
+                log.Debug("Search in path result: {Exe}", exe)
+                exe
+
+    if IO.Path.IsPathFullyQualified(cmd) then
+        log.Debug("'{Cmd}' is already fully qualified", cmd)
+        cmd
+    else
+        find cmd
 
 let runCmd (cmd: string) (args: string) =
     let proc = Diagnostics.Process.Start(cmd, args)
