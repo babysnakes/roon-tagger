6eab480a5ee96b4f1f8c3b299e7999ce6f9a8770
Haim Ashkenazi <haim@babysnakes.org>
2021-04-22 10:17:02 +0300

    A workflow to get the list of supported roles.
    From Roon's wiki.

--- README.md
+++ README.md
@@ -2,3 +2,21 @@
 
 Sometime in the future it will be a CLI utility for tagging music files with
 (mostly) roon specific metadata.
+
+### Development Setup
+
+Requires _.Net 5.0.x_.
+
+Run:
+```
+dotnet tool restore
+dotnet build
+```
+
+### Refreshing Roles List
+
+This app contains a complete list of supported roles in _Roon_'s credit tags (as
+published in their wiki). In order to update this roles list open
+`<REPOSITORY_ROOT>/src/Metadata.Lib/ExtractRoles.fsx` and follow the
+instructions. Note that this is not guaranteed to work, it's just how it worked
+for me.
---
+++ src/Metadata.Lib/ExtractRoles.fsx
@@ -0,0 +1,45 @@
+// This documents the process of extracting the supported roles of the Roon
+// wiki.
+
+// The first step is to point hour browser to:
+// https://help.roonlabs.com/portal/en/kb/articles/credit-roles
+//
+// This might take long to load. Open your browser's developer tools in the
+// *Network* tab and select 'xhr'. than save it locally under "bin\roles.json"
+// (relative to this file).
+
+// Send the file to FSI and then run: `run()` - This should update the saved
+// copy of `roles.txt`.
+
+#r "nuget: FSharp.Data, 4.1.0"
+
+open System.IO
+open FSharp.Data
+
+let basePath = __SOURCE_DIRECTORY__
+
+let rolesJson =
+    Path.Join([| basePath; "bin"; "roles.json" |])
+
+let resources =
+    Path.Join([| __SOURCE_DIRECTORY__; "Resources" |])
+
+let outputFile = Path.Join([| resources; "roles.txt" |])
+
+let run () =
+    let json = JsonValue.Load(rolesJson)
+    let rolesData = json.GetProperty "answer"
+    let html = HtmlDocument.Parse(rolesData.AsString())
+    let table = html.Descendants [ "table" ] |> Seq.head
+
+    let roles =
+        [
+          // First table row is titles
+          for row in table.Descendants [ "tr" ] |> Seq.tail do
+              for td in row.Descendants [ "td" ] do
+                  let t = td.InnerText().Trim()
+                  if t <> "" then yield t ]
+        |> List.sort
+
+    Directory.CreateDirectory resources |> ignore
+    File.WriteAllLines(outputFile, roles)
